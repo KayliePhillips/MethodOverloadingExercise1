1e5862a7f9795e72cbb41ff0ff5e5ab71d432ea1
Kaylie Phillips <kaylie.travel@gmail.com>
2022-09-16 10:45:45 -0500

    initial commit

---
+++ MethodOverloadingExercise1/Methods.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloadingExercise1
+{
+    internal class Methods
+    {
+        public static int Add(int num1, int num2)
+        {
+            return (num1 + num2);
+        }
+
+        public static decimal Add(decimal num1, decimal num2)
+        {
+            return (num1 + num2);
+        }
+
+        public static string Add(int num1, int num2, bool mybool)
+        {
+            if (mybool == true && (num1 + num2 > 1))
+            {
+                return ($"{num1 + num2} dollars");
+            }
+
+            else if (mybool == true && (num1 + num2 == 1))
+            {
+                  return ($"{num1 + num2} dollar");
+            }
+
+            else if (mybool == true && (num1 +num2 <1))
+            {
+                return ($"{num1 + num2} dollars");
+            }
+
+            else
+            {
+                return ("Numbers entered are not valid");
+            }
+        }
+    }
+}
---
+++ MethodOverloadingExercise1/Program.cs
@@ -0,0 +1,14 @@
+﻿using MethodOverloadingExercise1;
+
+
+Console.WriteLine(Methods.Add(0, 1)); //adding two integers
+
+Console.WriteLine(Methods.Add(5.32m, 6.33m)); //adding two decimals
+
+Console.WriteLine(Methods.Add(12, 6, true)); //3 parameters:2 integers, 1 boolean
+
+Console.WriteLine(Methods.Add(0, 1, true)); //3 parameters:2 integers, 1 boolean
+
+Console.WriteLine(Methods.Add(4, 2, false)); //3 parameters:2 integers, 1 boolean
+
+
